bf5a897de90a6a464de97f736a494ccf58377707
Wesley Monaro <wesleymonaro@hotmail.com>
2020-04-27 17:11:36 -0300

    separating routes

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -21,6 +21,7 @@ interface AuthState {
 interface AuthContextData {
   signIn(credentials: SignInCredentials): Promise<void>;
   user: object;
+  loading: boolean;
   signOut(): void;
 }
 
@@ -28,6 +29,7 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>({} as AuthState);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadStoragedData(): Promise<void> {
@@ -36,8 +38,10 @@ const AuthProvider: React.FC = ({ children }) => {
         '@GoBarber:user',
       ]);
 
-      if (token[1] && user[1])
+      if (token[1] && user[1]) {
         setData({ token: token[1], user: JSON.parse(user[1]) });
+      }
+      setLoading(false);
     }
     loadStoragedData();
   }, []);
@@ -63,7 +67,7 @@ const AuthProvider: React.FC = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut, loading }}>
       {children}
     </AuthContext.Provider>
   );
